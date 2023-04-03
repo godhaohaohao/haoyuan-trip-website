b751a1605007d7c595aa6280212bc4bbc45f98fd
hao <971536553@qq.com>
2023-04-03 17:12:25 +0800

    deploy

---
+++ assets/order-f5e6e948.js
@@ -0,0 +1,1 @@
+import{ab as e,ac as o,ad as s,ag as c}from"./index-58d6d92f.js";const r={},t={class:"order"},a=c("h2",null,"order",-1),n=[a];function _(d,i){return o(),s("div",t,n)}const f=e(r,[["render",_]]);export{f as default};
